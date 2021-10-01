ae17c0ead08012680dde9230ca5c929a460d3069
Michael DeSanty <michael.desanty@activatords.com>
2021-10-01 08:54:31 -0400

    Formatting

--- Sorting/BubbleSort/src/BubbleSort.java
+++ Sorting/BubbleSort/src/BubbleSort.java
@@ -4,7 +4,7 @@ public class BubbleSort {
 
   /* https://www.geeksforgeeks.org/bubble-sort/
    *
-   *Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in wrong order.
+   * Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in wrong order.
    * Example: 
    *
    * First Pass: 
